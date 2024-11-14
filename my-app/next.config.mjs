2a94e57610209f535c2857c96528ea39670a83b9
Jordan Pohr <97552850+jordanp2002@users.noreply.github.com>
2024-11-13 17:06:18 -0800

    hosting

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"export"
+};
+// next.config.ts or next.config.js
+
+
+
 
 export default nextConfig;
